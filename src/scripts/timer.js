00baaad41f3555f644bb0b33423553a725630b88
par1tet <mankova486@gmail.com>
2024-05-25 16:23:44 +0300

    main function

---
+++ src/scripts/timer.js
@@ -0,0 +1,34 @@
+sptimerSec = document.getElementById('sptimer-sec')
+sptimerMs = document.getElementById('sptimer-ms')
+point = document.getElementById('point')
+
+let isSolving = false;// Проверка на собирание кубика
+let timerInterval = 0;
+
+
+function timerLogic(event){
+    if (event.key !== ' '){// Смена по нажатию
+        console.log('timerjl')
+        return 0
+    }
+    if (!isSolving){// Если не собирает, и нажимает на пробел
+        point.innerHTML = "."
+        sptimerSec.innerHTML = "0";
+        sptimerMs.innerHTML = "0";
+        timerInterval = setInterval(() => {
+            if (sptimerMs.innerHTML == '99'){
+                sptimerSec.innerHTML = +sptimerSec.innerHTML + 1;
+                sptimerMs.innerHTML = 0;
+            }else{
+                sptimerMs.innerHTML = +sptimerMs.innerHTML + 1;
+            }
+        },10)
+    }else{
+        clearInterval(timerInterval)
+    }
+    if (event.key === ' '){// Смена по нажатию
+        isSolving = !isSolving;
+    }
+}
+
+document.addEventListener('keydown', timerLogic)
